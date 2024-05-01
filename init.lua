8d060f8033a2c58c11f168df156f3af602d7b7d2
kaBeech <contact@kaBeech.com>
2024-04-30 22:40:17 -0700

    Initialize spell checking

--- init.lua
+++ init.lua
@@ -98,6 +98,11 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- Enable spell checking
+
+vim.opt.spell = true
+vim.opt.spelllang = 'en_us'
+
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, for help with jumping.
